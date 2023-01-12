650aa7c4585780d7089bda4fca7d2b7e50d9db8d
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-01-11 18:23:03 -0600

    feat(autopairs): improved the `autopairs` plugin setup

--- lua/wuelner/settings/autopairs.lua
+++ lua/wuelner/settings/autopairs.lua
@@ -1,5 +1,29 @@
 local config = function()
-  require("nvim-autopairs").setup({ check_ts = true })
+  require("nvim-autopairs").setup({
+    disable_filetype = {
+      "aerial",
+      "checkhealth",
+      "dapui_breakpoints",
+      "dapui_console",
+      "dapui_scopes",
+      "dapui_stacks",
+      "fern",
+      "lspinfo",
+      "mason",
+      "nerdterm",
+      "noice",
+      "notify",
+      "null-ls-info",
+      "packer",
+      "qf",
+      "TelescopePrompt",
+    },
+    enable_check_bracket_line = false,
+    check_ts = true,
+    map_cr = true,
+    fast_wrap = { highlight = "Question", highlight_grey = "Dimmed" },
+  })
+
   require("cmp").event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
 end
 
