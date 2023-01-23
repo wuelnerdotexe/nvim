352042d94d8b8c84339b49961c56387b6b3cbe15
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-01-23 16:47:32 -0600

    feat(mason): added improvements to `mason.nvim` plugin

--- lua/wuelner/plugins/mason.lua
+++ lua/wuelner/plugins/mason.lua
@@ -1,12 +1,12 @@
 return {
   "williamboman/mason.nvim",
   lazy = true,
+  cmd = "Mason",
   config = function()
     require("mason").setup({
       ui = {
         border = "rounded",
         icons = { package_installed = "✓", package_pending = "⟳", package_uninstalled = "-" },
-        keymaps = { uninstall_package = "d" },
       },
     })
   end,
