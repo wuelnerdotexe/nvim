96f2e2058c045798eb1f4fee0d4786a975476f8e
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-04-13 17:43:01 -0600

    feat(plugin): enable tmux `copy_sync` with yank

--- lua/wuelnerdotexe/plugin/opt/yanky.lua
+++ lua/wuelnerdotexe/plugin/opt/yanky.lua
@@ -1,14 +1,108 @@
-local mode = { "n", "x" }
-
 return {
   "gbprod/yanky.nvim",
   enabled = not require("wuelnerdotexe.plugin.config").minimal_setup,
   keys = {
-    { "y", "<Plug>(YankyYank)", mode = mode },
-    { "p", "<Plug>(YankyPutAfter)", mode = mode },
-    { "P", "<Plug>(YankyPutBefore)", mode = mode },
-    { "gp", "<Plug>(YankyGPutAfter)", mode = mode },
-    { "gP", "<Plug>(YankyGPutBefore)", mode = mode },
+    { "y", "<Plug>(YankyYank)", mode = { "n", "x" } },
+    {
+      "p",
+      function()
+        if vim.env.TMUX then
+          local ok, tmux_copy = pcall(require, "tmux.copy")
+
+          if ok then tmux_copy.sync_registers() end
+        end
+
+        require("yanky").put("p", false)
+      end,
+    },
+    {
+      "P",
+      function()
+        if vim.env.TMUX then
+          local ok, tmux_copy = pcall(require, "tmux.copy")
+
+          if ok then tmux_copy.sync_registers() end
+        end
+
+        require("yanky").put("P", false)
+      end,
+    },
+    {
+      "gp",
+      function()
+        if vim.env.TMUX then
+          local ok, tmux_copy = pcall(require, "tmux.copy")
+
+          if ok then tmux_copy.sync_registers() end
+        end
+
+        require("yanky").put("gp", false)
+      end,
+    },
+    {
+      "gP",
+      function()
+        if vim.env.TMUX then
+          local ok, tmux_copy = pcall(require, "tmux.copy")
+
+          if ok then tmux_copy.sync_registers() end
+        end
+
+        require("yanky").put("gP", false)
+      end,
+    },
+    {
+      "p",
+      function()
+        if vim.env.TMUX then
+          local ok, tmux_copy = pcall(require, "tmux.copy")
+
+          if ok then tmux_copy.sync_registers() end
+        end
+
+        require("yanky").put("p", true)
+      end,
+      mode = "x",
+    },
+    {
+      "P",
+      function()
+        if vim.env.TMUX then
+          local ok, tmux_copy = pcall(require, "tmux.copy")
+
+          if ok then tmux_copy.sync_registers() end
+        end
+
+        require("yanky").put("P", true)
+      end,
+      mode = "x",
+    },
+    {
+      "gp",
+      function()
+        if vim.env.TMUX then
+          local ok, tmux_copy = pcall(require, "tmux.copy")
+
+          if ok then tmux_copy.sync_registers() end
+        end
+
+        require("yanky").put("gp", true)
+      end,
+      mode = "x",
+    },
+    {
+      "gP",
+      function()
+        if vim.env.TMUX then
+          local ok, tmux_copy = pcall(require, "tmux.copy")
+
+          if ok then tmux_copy.sync_registers() end
+        end
+
+        require("yanky").put("gP", true)
+      end,
+      mode = "x",
+    },
     { "<C-Left>", "<Plug>(YankyCycleBackward)" },
     { "<C-Right>", "<Plug>(YankyCycleForward)" },
   },
--- lua/wuelnerdotexe/plugin/start/tmux.lua
+++ lua/wuelnerdotexe/plugin/start/tmux.lua
@@ -3,9 +3,26 @@ return {
   keys = { "<C-h>", "<C-j>", "<C-k>", "<C-l>", "<A-h>", "<A-j>", "<A-k>", "<A-l>" },
   config = function()
     require("tmux").setup({
-      copy_sync = { enable = false },
-      navigation = { cycle_navigation = true, enable_default_keybindings = true },
-      resize = { enable_default_keybindings = true, resize_step_x = 1, resize_step_y = 1 },
+      copy_sync = {
+        enable = true,
+        ignore_buffers = { empty = false },
+        redirect_to_clipboard = false,
+        register_offset = 0,
+        sync_clipboard = true,
+        sync_registers = true,
+        sync_deletes = true,
+        sync_unnamed = true,
+      },
+      navigation = {
+        cycle_navigation = true,
+        enable_default_keybindings = true,
+        persist_zoom = false,
+      },
+      resize = {
+        enable_default_keybindings = true,
+        resize_step_x = 1,
+        resize_step_y = 1,
+      },
     })
   end,
 }
