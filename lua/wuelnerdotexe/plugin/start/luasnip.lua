8a06e614fbbcc5adb87358f6e6bd33e0815170e0
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-04-30 00:28:00 -0600

    feat(luasnip): add/integrate `tabout` plugin

--- lua/wuelnerdotexe/plugin/start/luasnip.lua
+++ lua/wuelnerdotexe/plugin/start/luasnip.lua
@@ -4,21 +4,43 @@ return {
   keys = {
     {
       "<Tab>",
-      function() return require("luasnip").expand_or_jumpable() and "<Plug>luasnip-expand-or-jump" or "<Tab>" end,
+      function() return require("luasnip").expand_or_jumpable() and "<Plug>luasnip-expand-or-jump" or "<Plug>(Tabout)" end,
       expr = true,
       mode = "i",
     },
     { "<Tab>", function() require("luasnip").jump(1) end, mode = "s" },
-    { "<S-Tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    {
+      "<S-Tab>",
+      function() return require("luasnip").jumpable(-1) and "<Plug>luasnip-jump-prev" or "<Plug>(TaboutBack)" end,
+      expr = true,
+      mode = "i",
+    },
+    { "<S-Tab>", function() require("luasnip").jump(-1) end, mode = "s" },
   },
-  dependencies = "rafamadriz/friendly-snippets",
+  dependencies = {
+    {
+      "rafamadriz/friendly-snippets",
+      config = function() require("luasnip.loaders.from_vscode").lazy_load() end,
+    },
+    {
+      "abecodes/tabout.nvim",
+      dependencies = "nvim-treesitter/nvim-treesitter",
+      config = function()
+        require("tabout").setup({
+          tabkey = "",
+          backwards_tabkey = "",
+          ignore_beginning = false,
+          exclude = require("wuelnerdotexe.plugin.config").uifiletypes,
+        })
+      end,
+    },
+    "nvim-treesitter/nvim-treesitter",
+  },
   config = function()
     require("luasnip").setup({
       history = true,
       region_check_events = { "CursorMoved", "CursorMovedI" },
       delete_check_events = { "TextChanged", "TextChangedI" },
     })
-
-    require("luasnip.loaders.from_vscode").lazy_load()
   end,
 }
