9b13c08cb6b06de20794a6b0e46bbc6ce6197cb8
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-05-14 19:15:19 -0600

    chore(lazy-lock): update plugins

--- lua/wuelnerdotexe/plugin/start/mason.lua
+++ lua/wuelnerdotexe/plugin/start/mason.lua
@@ -1,5 +1,6 @@
 return {
   "williamboman/mason.nvim",
+  build = ":MasonUpdate",
   lazy = true,
   config = function()
     require("mason").setup({ ui = { border = require("wuelnerdotexe.plugin.config").border and "rounded" or "none" } })
