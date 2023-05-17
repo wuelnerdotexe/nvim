51babb8f8082ff6022ed63a370d0a9257aefb005
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-05-16 20:19:07 -0600

    feat(muren): add `muren` plugin

---
+++ lua/wuelnerdotexe/plugin/start/muren.lua
@@ -0,0 +1,17 @@
+return {
+  "AckslD/muren.nvim",
+  cmd = { "MurenOpen", "MurenClose", "MurenToggle", "MurenFresh", "MurenUnique" },
+  config = function()
+    require("muren").setup({
+      keys = {
+        toggle_options_focus = "<C-o>",
+        scroll_preview_up = "<C-b>",
+        scroll_preview_down = "<C-f>",
+      },
+      hl = {
+        options = { on = "Success", off = "Error" },
+        preview = { cwd = { path = "Directory", lnum = "LineNr" } },
+      },
+    })
+  end,
+}
