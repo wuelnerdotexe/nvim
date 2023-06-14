1ba7cd91bafa9c94eea64896eb704a8e34c12ddd
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-06-14 16:59:15 -0600

    style(util): format code with stylua

--- lua/wuelnerdotexe/plugin/util.lua
+++ lua/wuelnerdotexe/plugin/util.lua
@@ -1,7 +1,7 @@
 local args
 local sidebar_width
 
-local M = { empty_table = { }, plugin_options = { } }
+local M = { empty_table = {}, plugin_options = {} }
 
 M.set_option = function(option, value)
   M.plugin_options[option] = vim.api.nvim_get_option_value(option, M.empty_table)
