423949c6553c756bd6df47c0d17356a06f6d1722
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-07-24 23:18:11 -0600

    fix(comment): improve `commentstring` setup

--- lua/wuelnerdotexe/plugin/start/comment.lua
+++ lua/wuelnerdotexe/plugin/start/comment.lua
@@ -1,18 +1,23 @@
 return {
-  "numToStr/Comment.nvim",
-  dependencies = {
+  {
+    "numToStr/Comment.nvim",
+    keys = { { "gc", mode = { "n", "x" } } },
+    config = function()
+      require("Comment").setup({
+        toggler = { line = "gcc", block = "<Nop>" },
+        opleader = { line = "gc", block = "<Nop>" },
+        pre_hook = require("lazy.core.config").spec.plugins["nvim-ts-context-commentstring"] and function(ctx)
+          return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(ctx)
+        end or nil,
+      })
+    end,
+  },
+  {
     "JoosepAlviste/nvim-ts-context-commentstring",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       opts = { context_commentstring = { enable = true, enable_autocmd = false } },
     },
+    lazy = true,
   },
-  keys = { { "gc", mode = { "n", "x" } } },
-  config = function()
-    require("Comment").setup({
-      toggler = { line = "gcc", block = "<Nop>" },
-      opleader = { line = "gc", block = "<Nop>" },
-      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-    })
-  end,
 }
