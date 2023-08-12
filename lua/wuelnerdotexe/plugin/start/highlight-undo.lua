b621dd7db4ac848e6fbd29c2612b5052054c324a
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-08-11 21:11:18 -0600

    fix(highlight-undo): update setup

--- lua/wuelnerdotexe/plugin/start/highlight-undo.lua
+++ lua/wuelnerdotexe/plugin/start/highlight-undo.lua
@@ -1,11 +1,11 @@
 return {
   "tzachar/highlight-undo.nvim",
   keys = { "u", "<C-r>" },
-  init = function() require("wuelnerdotexe.plugin.util").add_colorscheme_integration("highlight-undo") end,
   config = function()
     require("highlight-undo").setup({
       duration = 125,
-      keymaps = { { "n", "u", "undo", {} }, { "n", "<C-r>", "redo", {} } },
+      undo = { hlgroup = "DiagnosticVirtualTextError" },
+      redo = { hlgroup = "DiagnosticVirtualTextOk" },
     })
   end,
 }
