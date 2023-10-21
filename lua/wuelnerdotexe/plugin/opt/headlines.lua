34750eccc8deb8c89766891008062c17a43e2471
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-10-20 22:38:52 -0600

    fix(headlines): some hot fixes

--- lua/wuelnerdotexe/plugin/opt/headlines.lua
+++ lua/wuelnerdotexe/plugin/opt/headlines.lua
@@ -1,7 +1,12 @@
 return {
   "lukas-reineke/headlines.nvim",
   dependencies = "nvim-treesitter/nvim-treesitter",
-  ft = { "markdown", "orgmode", "neorg" },
+  ft = { "markdown", "org", "norg" },
   init = function() require("wuelnerdotexe.plugin.util").add_colorscheme_integration("headlines") end,
-  config = function() require("headlines").setup() end,
+  config = function()
+    local headline_highlights =
+      { headline_highlights = { "Headline1", "Headline2", "Headline3", "Headline4", "Headline5", "Headline6" } }
+
+    require("headlines").setup({ markdown = headline_highlights, org = headline_highlights, norg = headline_highlights })
+  end,
 }
