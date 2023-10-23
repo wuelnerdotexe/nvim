1d7986025d8fec72fed0c6948330ccc009f1f699
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-10-22 19:53:09 -0600

    fix(tabnine): minor improvement

--- lua/wuelnerdotexe/plugin/opt/tabnine.lua
+++ lua/wuelnerdotexe/plugin/opt/tabnine.lua
@@ -7,16 +7,14 @@ return {
   cmd = { "TabnineChat", "TabnineDisable", "TabnineEnable", "TabnineHub", "TabnineStatus", "TabnineToggle" },
   event = { "BufEnter", "FileType" },
   config = function()
-    local get_scheme_color = require("wuelnerdotexe.plugin.util").get_scheme_color
-
     require("tabnine").setup({
       disable_auto_comment = true,
       accept_keymap = "<C-]>",
       dismiss_keymap = "<C-[>",
       debounce_ms = vim.api.nvim_get_option_value("timeoutlen", { scope = "global" }),
       suggestion_color = {
-        gui = get_scheme_color("LspCodeLens", "fg"),
-        cterm = get_scheme_color("LspCodeLens", "ctermfg"),
+        gui = require("wuelnerdotexe.plugin.util").get_scheme_color("LspCodeLens", "fg"),
+        cterm = require("wuelnerdotexe.plugin.util").get_scheme_color("LspCodeLens", "ctermfg"),
       },
       exclude_filetypes = require("wuelnerdotexe.plugin.util").user_interface_filetypes,
     })
