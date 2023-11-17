efb4c0ec777e45524c29847b467faa65cf685554
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-11-17 17:54:36 -0600

    fix(animate): minor improvement are madded

--- lua/wuelnerdotexe/plugin/opt/animate.lua
+++ lua/wuelnerdotexe/plugin/opt/animate.lua
@@ -4,7 +4,7 @@ return {
   init = function() require("wuelnerdotexe.plugin.util").add_colorscheme_integration("animate") end,
   config = function()
     require("mini.animate").setup({
-      open = { open = false },
+      open = { enable = not require("lazy.core.config").spec.plugins["nvim-spectre"] },
       cursor = { timing = function(_, n) return 125 / n end },
       scroll = { timing = function(_, n) return 67 / n end },
       resize = { timing = function(_, n) return 67 / n end },
