09372c2320a79cb89222665eb390d480a539c8df
Wuelner Martínez <wuelner.martinez@outlook.com>
2023-11-30 14:47:54 -0600

    fix(comment): avoid deprecateds

--- lua/wuelnerdotexe/plugin/start/comment.lua
+++ lua/wuelnerdotexe/plugin/start/comment.lua
@@ -6,16 +6,16 @@ return {
       require("Comment").setup({
         toggler = { line = "gcc", block = "<Nop>" },
         opleader = { line = "gc", block = "<Nop>" },
-        pre_hook = function(...) return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(...) end,
+        pre_hook = function(...)
+          return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(...)
+        end,
       })
     end,
   },
   {
     "JoosepAlviste/nvim-ts-context-commentstring",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      opts = { context_commentstring = { enable = true, enable_autocmd = false } },
-    },
     lazy = true,
+    init = function() vim.api.nvim_set_var("skip_ts_context_commentstring_module", true) end,
+    config = function() require("ts_context_commentstring").setup({ enable_autocmd = false }) end,
   },
 }
